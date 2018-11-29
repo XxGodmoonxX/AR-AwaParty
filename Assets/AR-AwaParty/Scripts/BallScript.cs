1972224bc7a554c515b7cbbd1685c7034b3e69f9
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-11-29 20:08:48 +0900

    ボールの初期設定、座標とか重力とか

--- Assets/AR-AwaParty/Scripts/BallScript.cs
+++ Assets/AR-AwaParty/Scripts/BallScript.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class BallScript : MonoBehaviour {
 
 	//PublicでGameobject型の変数
+	public GameObject ball_blue;
+	public GameObject ball_red;
+
+	//float型の変数pos
+	private float pos_blue;
+	private float pos_red;
 
 	// Use this for initialization
 	void Start () {
-		
+		//posにballのy座標を格納 ボールは上下にバウンド、yの値を取得しておく必要がある
+		pos_blue = ball_blue.transform.position.y;
+		pos_red = ball_red.transform.position.y;
+
+		//ballに与えられているRigidbodyのuseGravityをFalse
+		//これでボールには重力が働かず宙に浮いた状態に
+		ball_blue.GetComponent<Rigidbody>().useGravity = false;
+		ball_red.GetComponent<Rigidbody>().useGravity = false;
 	}
 	
 	// Update is called once per frame
