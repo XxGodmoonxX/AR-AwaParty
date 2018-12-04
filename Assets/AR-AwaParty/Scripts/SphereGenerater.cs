f37ce08b344af285a03e1de45fb6c02bc0307acc
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-12-04 18:43:46 +0900

    ボールが発生

--- Assets/AR-AwaParty/Scripts/SphereGenerater.cs
+++ Assets/AR-AwaParty/Scripts/SphereGenerater.cs
@@ -7,7 +7,7 @@ namespace UnityEngine.XR.iOS {
 
 		public GameObject ballPrefab;
 
-		public Camera cam;
+		// public Camera cam;
 
 		// Use this for initialization
 		void Start () {
@@ -27,17 +27,19 @@ namespace UnityEngine.XR.iOS {
 			//ランダムに変化する色をCubeに適用する
       sphere.GetComponent<Renderer>().material = material;
 
-			sphere.transform.position = ballPrefab.transform.TransformPoint(0, 0.5f, -0.2f);
+			sphere.transform.position = ballPrefab.transform.TransformPoint(0, 0, 0);
 			// sphere.transform.position = ballPrefab.transform.position;
-			
+
 
 			//sphereの大きさ指定
 			float sphereSize = 0.5f;
       sphere.transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
-      //CsphereにはRigidbodyを持たせて重力を与えておかないと、床の上には配置されないので注意が必要。Rigidbodyで重力を持たせないとCubeは宙に浮いた状態になる
+      //sphereにはRigidbodyを持たせて重力を与えておかないと、床の上には配置されないので注意が必要。Rigidbodyで重力を持たせないとCubeは宙に浮いた状態になる
       sphere.AddComponent<Rigidbody>();
-			//これで斜め上にキューブ飛んでいく？そして衝突計算
-      sphere.GetComponent<Rigidbody>().AddForce(sphere.transform.TransformDirection(0,1f,2f),ForceMode.Impulse);
+
+			sphere.GetComponent<Rigidbody>().AddForce(0, 0, 0);
+
+      // sphere.GetComponent<Rigidbody>().AddForce(sphere.transform.TransformDirection(0,0,0),ForceMode.Impulse);
 			// sphere.GetComponent<Rigidbody>().AddForce(cam.transform.TransformDirection(0,0,0),ForceMode.Impulse);
 
 		}
