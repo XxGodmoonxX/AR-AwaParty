40d4e65c87e303ca3b55ea11c41b95d5a5a7effb
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-12-04 21:56:41 +0900

    なんだっけこれ

--- Assets/AR-AwaParty/Scripts/SphereGenerater.cs
+++ Assets/AR-AwaParty/Scripts/SphereGenerater.cs
@@ -27,21 +27,15 @@ namespace UnityEngine.XR.iOS {
 			//ランダムに変化する色をCubeに適用する
       sphere.GetComponent<Renderer>().material = material;
 
-			sphere.transform.position = ballPrefab.transform.TransformPoint(0, 0, 0);
-			// sphere.transform.position = ballPrefab.transform.position;
-
-
 			//sphereの大きさ指定
 			float sphereSize = 0.5f;
       sphere.transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
-      //sphereにはRigidbodyを持たせて重力を与えておかないと、床の上には配置されないので注意が必要。Rigidbodyで重力を持たせないとCubeは宙に浮いた状態になる
+
+      //sphereにはRigidbodyを持たせて重力を与えておかないと、床の上には配置されないので注意が必要。Rigidbodyで重力を持たせないと宙に浮いた状態になる
+			// Rigidbodyを取得
       sphere.AddComponent<Rigidbody>();
-
-			sphere.GetComponent<Rigidbody>().AddForce(0, 0, 0);
-
-      // sphere.GetComponent<Rigidbody>().AddForce(sphere.transform.TransformDirection(0,0,0),ForceMode.Impulse);
-			// sphere.GetComponent<Rigidbody>().AddForce(cam.transform.TransformDirection(0,0,0),ForceMode.Impulse);
-
+			// ボール発生位置をballprefab = sphereの位置に変更
+			sphere.GetComponent<Rigidbody>().position = ballPrefab.transform.position;
 		}
 	}
 }
