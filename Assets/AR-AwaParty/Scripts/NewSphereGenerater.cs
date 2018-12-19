f7e298258e5d9d20e25488ad101d258ba8767183
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-12-20 03:43:57 +0900

    クラブ実験用のFix

--- Assets/AR-AwaParty/Scripts/NewSphereGenerater.cs
+++ Assets/AR-AwaParty/Scripts/NewSphereGenerater.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnityEngine.XR.iOS {
-	public class SphereGenerater : MonoBehaviour {
+	public class NewSphereGenerater : MonoBehaviour {
 
 		public GameObject ballPrefab;
 
@@ -27,7 +27,7 @@ namespace UnityEngine.XR.iOS {
       sphere.GetComponent<Renderer>().material = material;
 
 			//sphereの大きさ指定
-			float sphereSize = 0.5f;
+			float sphereSize = 1.0f;
       sphere.transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
 
       //sphereにはRigidbodyを持たせて重力を与えておかないと、床の上には配置されないので注意が必要。Rigidbodyで重力を持たせないと宙に浮いた状態になる
@@ -41,7 +41,7 @@ namespace UnityEngine.XR.iOS {
 
 			// ボール発生位置をballprefab = sphereの位置に変更
 			// からのY方向に球を発射
-			sphere.transform.position = ballPrefab.transform.TransformPoint(0, 0.5f, -0.2f);
+			sphere.transform.position = ballPrefab.transform.TransformPoint(Random.Range(-0.2f, 0.2f), 0.1f, -0.1f);
 
 			//力を加える？
 			// sphere.GetComponent<Rigidbody>().AddForce(0, 0, 0);
---
+++ Assets/AR-AwaParty/Scripts/ScoreManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public GameObject score_object = null; //Text
+	// public GameObject score_object_final = null; //Text
+
+
+	private float  MaxTime = 60f; //秒
+
+	// private float acx_x = 0;
+	// private	float acx_y = 0;
+	// private float acx_z = 0;
+	private float acxNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		Text score_text = score_object.GetComponent<Text>();
+		acxNum = 0;
+		score_text.text = acxNum.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var acx = Vector3.zero;
+		acx.x += Mathf.Abs(Input.acceleration.x);
+		acx.y += Mathf.Abs(Input.acceleration.y);
+		acx.z += Mathf.Abs(Input.acceleration.z);
+
+		acxNum = Mathf.Abs(acx.x + acx.y + acx.z);
+		// acx += 0.00001f;
+		Text score_text = score_object.GetComponent<Text>();
+		score_text.text = acxNum.ToString();
+
+		if(Time.time > 5000) {
+			// acx = acx_x + acx_y + acx_z + 1.0f;
+			//オブジェクトからTextコンポーネント取得
+			// Text score_text = score_object.GetComponent<Text>();
+			//テキスト入れ替える
+			// score_text.text = acx.ToString();
+		}
+
+
+		if(Time.time == MaxTime*1000) {
+			// acx = acx_x + acx_y + acx_z;
+			//オブジェクトからTextコンポーネント取得
+			// Text score_text = score_object.GetComponent<Text>();
+			//テキスト入れ替える
+			// score_text.text = acx.ToString();
+		}
+	}
+}
