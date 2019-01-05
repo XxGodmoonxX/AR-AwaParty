27fae4539e504ddc7e62a129277f76296da758bc
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-05 15:05:58 +0900

    入力をマイクに

--- Assets/AR-AwaParty/Scripts/sound.cs
+++ Assets/AR-AwaParty/Scripts/sound.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sounda : MonoBehaviour {
+public class sound : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		AudioSource aud = GetComponent<AudioSource>();
+    // マイク名、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
+    aud.clip = Microphone.Start(null, true, 10, 44100);
+    aud.Play();
 	}
 	
 	// Update is called once per frame
