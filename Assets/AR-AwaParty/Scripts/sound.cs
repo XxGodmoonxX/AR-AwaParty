8534af0422faa4c0c57ddd0f94e2041ddf1c40bd
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-07 17:24:14 +0900

    マイク入力の諸々Fix

--- Assets/AR-AwaParty/Scripts/sound.cs
+++ Assets/AR-AwaParty/Scripts/sound.cs
@@ -7,13 +7,34 @@ public class sound : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		AudioSource aud = GetComponent<AudioSource>();
-    // マイク名、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
+		// Audio Source の Audio Clip をマイク入力に設定
+    // マイク名nullならデフォルト、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
     aud.clip = Microphone.Start(null, true, 10, 44100);
+		// マイクが Ready になるまで待機（一瞬）
+		while (Microphone.GetPosition(null) <= 0) {}
+		// 再生開始（録った先から再生、スピーカーから出力するとハウリングします）
     aud.Play();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		// float[] spectrum = new float[1024];
+    // AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+		// Debug.Log(spectrum[1]);
+
+		// //ピッチの取得
+		// var maxIndex = 0;
+		// var maxValue = 0.0f;
+		// for (int i = 0; i <= spectrum.Length; i++) {
+		// 	var val = spectrum[i];
+		// 	if (val > maxValue) {
+		// 		// maxValue が最も大きい周波数成分の値で
+		// 		maxValue = val;
+		// 		// maxIndex がそのインデックス。欲しいのはこっち。
+		// 		maxIndex = i;
+		// 	}
+		// }
+
+		// var freq = maxIndex * AudioSettings.outputSampleRate / 2 / spectrum.Length;
 	}
 }
