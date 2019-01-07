e6214af82ceef315fa39a5da861e71f2054c9336
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-07 18:44:11 +0900

    変数作成

--- Assets/AR-AwaParty/Scripts/SoundVisual.cs
+++ Assets/AR-AwaParty/Scripts/SoundVisual.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class SoundVisual : MonoBehaviour {
 
+	public AudioSource source;
+	private float[] samples;
+	private float[] spectrum;
+	private float sampleRate;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
